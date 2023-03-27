6e7fd91248d77f4b210ff966b86234d30abd8fd4
pritamy8062 <120549375+pritamy8062@users.noreply.github.com>
2023-03-27 22:22:37 +0530

    Update Form.jsx

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -86,7 +86,7 @@ const Form = () => {
     };
 
     const login = async (values, onSubmitProps) => {
-        const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+        const loggedInResponse = await fetch("https://sociopedia-backend-zdsw.onrender.com/auth/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(values),
